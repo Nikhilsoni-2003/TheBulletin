1a7cc7adf3db275e0a9981f685110396cc9aab15
Nikhil Soni <124785555+Nikhilsoni-2003@users.noreply.github.com>
2025-08-15 18:20:54 +0000

    vite config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_ORIGIN || 'http://localhost:5000', 
+        target: import.meta.env.VITE_API_BASE || 'http://localhost:5000', 
         changeOrigin: true,
       },
     },
